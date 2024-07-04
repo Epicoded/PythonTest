112282c9ed706c567559154023ca3539cbb21bc3
Epicode <40544619+Epicoded@users.noreply.github.com>
2024-07-03 19:59:13 -0600

    Delete dos.py

--- dos.py
+++
@@ -1,7 +0,0 @@
-import matplotlib
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-plt.savefig('test.png')  # En lugar de plt.show()
-print("Gráfico guardado como 'test.png'")
